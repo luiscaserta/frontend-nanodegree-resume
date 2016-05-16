adf24d53c6304874ab69945fa54274c0e61004e9
luiscaserta <luiscaserta@yahoo.com>
2016-05-16 14:43:33 -0400

    Review 1
    Initial modifications after review of Stage 5 problem sets

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,74 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+/*
+$("#main").append("Luis Caserta");
+var awesomeThoughts = "I am Luis Caserta and I am LEARNING";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("LEARNING", "STRETCHING IN THE LEARNING ZONE");
+$("#main").append(funThoughts);
+*/
+/*
+var formattedName = HTMLheaderName.replace("%data%", "Luis Caserta");
+//console.log(formattedName)
+var formattedRole = HTMLheaderRole.replace("%data%", "Student Web Developer")
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+*/
+/*
+var skills = ["nice", "smart", "happy"];
+var bio = {
+	"name" : "Luis",
+	"role" : "The Student",
+	"contactInfo" : {
+		"mobile" : "514-641-4279",
+		"email" : "luiscaserta@yahoo.com",
+		"github" : "luiscaserta",
+		"twitter" : "@luiscaserta",
+		"location" : "Montreal"
+	},
+	"welcomeMessage" : "Let's have some fun - learn, teach and grow",
+	"skills" : skills,
+	"bioPic" : "GitHub/frontend-nanodegree-resume/images/fry.jpg"
+};
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contactInfo);
+$("#main").append(bio.welcomeMessage);
+$("#main").append(bio.skills);
+$("#main").append(bio.bioPic);
+*/
+/*
+var work = {};
+work.currentJobPosition = "ESL Teacher";
+work.employer = "Consultant";
+work.years = 1;
+work.city = "Montreal";
+
+$("#main").append(work.currentJobPosition);
+
+var education = {};
+education["schoolName"] = "ILSC - Montreal";
+education["years"] = 1;
+education["city"] = "Montreal";
+
+$("#main").append(education["schoolName"]);
+*/
+
+var education = {
+	"schools" : [
+		{
+			"name" : "ILSC - Montreal",
+			"city" : "Montreal",
+			"degree" : "DELTA / ESL Certificate"
+		},
+		{
+			"name" : "Udacity",
+			"city" : "On-line",
+			"degree" : "IPND"	
+		}
+	]
+}
+
