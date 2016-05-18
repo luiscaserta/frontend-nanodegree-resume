74328b32e36af03fa679e5538bb646ac26eaf298
luiscaserta <luiscaserta@yahoo.com>
2016-05-18 13:17:05 -0400

    revision 2
    updates

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,74 +1,112 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+Here is my JavaScript Code for final project to build the resume.
 */
-/*
-$("#main").append("Luis Caserta");
-var awesomeThoughts = "I am Luis Caserta and I am LEARNING";
-console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace("LEARNING", "STRETCHING IN THE LEARNING ZONE");
-$("#main").append(funThoughts);
-*/
-/*
-var formattedName = HTMLheaderName.replace("%data%", "Luis Caserta");
-//console.log(formattedName)
-var formattedRole = HTMLheaderRole.replace("%data%", "Student Web Developer")
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
-*/
-/*
-var skills = ["nice", "smart", "happy"];
 var bio = {
-	"name" : "Luis",
-	"role" : "The Student",
-	"contactInfo" : {
-		"mobile" : "514-641-4279",
-		"email" : "luiscaserta@yahoo.com",
-		"github" : "luiscaserta",
-		"twitter" : "@luiscaserta",
-		"location" : "Montreal"
+	"name": "Luis Caserta",
+	"role": "Front End Web Developer - in Training!!",
+	"contactInfo": {
+		"mobile": "514-641-4279",
+		"email": "luiscaserta@yahoo.com",
+		"github": "luiscaserta",
+		"twitter": "notwitter@luiscaserta",
+		"location": "Montreal"
 	},
-	"welcomeMessage" : "Let's have some fun - learn, teach and grow",
-	"skills" : skills,
-	"bioPic" : "GitHub/frontend-nanodegree-resume/images/fry.jpg"
-};
+	"welcomeMessage": "Let's have some fun - learn, teach and grow",
+	"skills": [
+		"Languages: English, French and Spanish", "Friendly and Easy to Work with", "Persistent" 
+	],
+	"bioPic" : "images/fry.jpg"
+}
 
-$("#main").append(bio.name);
-$("#main").append(bio.role);
-$("#main").append(bio.contactInfo);
-$("#main").append(bio.welcomeMessage);
-$("#main").append(bio.skills);
-$("#main").append(bio.bioPic);
-*/
-/*
-var work = {};
-work.currentJobPosition = "ESL Teacher";
-work.employer = "Consultant";
-work.years = 1;
-work.city = "Montreal";
 
-$("#main").append(work.currentJobPosition);
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
 
-var education = {};
-education["schoolName"] = "ILSC - Montreal";
-education["years"] = 1;
-education["city"] = "Montreal";
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill);
 
-$("#main").append(education["schoolName"]);
-*/
+	console.log(HTMLskillsStart);
+	console.log(HTMLskills);
+	console.log(formattedSkill);
+}
 
 var education = {
-	"schools" : [
+	"schools": [
 		{
-			"name" : "ILSC - Montreal",
-			"city" : "Montreal",
-			"degree" : "DELTA / ESL Certificate"
+			"name": "University of Detroit Mercy",
+			"city": "Detroit, MI", 
+			"degree": "Bachelors", 
+			"majors": "Mechanical Engineering", 
+			"dates": 1998, 
+			"url": "www.udmercy.edu"
 		},
 		{
-			"name" : "Udacity",
-			"city" : "On-line",
-			"degree" : "IPND"	
+			"name": "ILSC - Montreal",
+			"city": "Montreal, Quebec", 
+			"degree": "ESL", 
+			"majors": "DELTA", 
+			"dates": 2014, 
+			"url": "www.ilsc.com/montreal"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Introduction to Programming Nano Degree",
+			"school": "Udacity",
+			"dates": 2016
 		}
 	]
 }
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Private Consultant",
+			"title": "ESL Teacher",
+			"location": "Montreal",
+			"dates": "2015 - present",
+			"descritpion": "Private English language instructor."
+		},
+		{
+			"employer": "Hatch",
+			"title": "Contract Administrator",
+			"location": "New Caledonia",
+			"dates": "2010 - 2014",
+			"descritpion": "EPCM of both local and international contractors for execution of Koniambo Nickel Construction Projects."
+		}
+	]
+}
+/*
+for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+	$(".work-entry:last").append(formattedEmployerTitle);
+}
+*/
+
+var projects = {
+	"projects": [
+		{
+			"title": "Family Project",
+			"dates": "2001 - present",
+			"description": "Happy Family............ mommy, daddy, and the two little ones",
+			"images": [
+				"https://get some pics",
+				"https://get some pics"
+			]
+		}
+	]
+}
+
+
+
 
